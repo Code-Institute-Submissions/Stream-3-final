ca9d2646f793790bd38e3c86a6f5cfce9bba7611
John <arnold-j83@sky.com>
2017-02-07 18:33:35 +0000

    tuesday

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.utils import timezone
 
 def get_index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[0]
+    args = {'posts': posts}
+    return render(request, 'index.html', args)
