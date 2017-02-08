cc1af6ffd74afceb8fe74777220f9a92205c4860
John <arnold-j83@sky.com>
2017-02-08 15:23:27 +0000

    tuesday

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from blog.models import Post
+from threads.models import Subject, Thread
 from django.utils import timezone
 
 def get_index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[0]
-    print "posts:",posts
-    args = {'posts': posts}
+    forum_subjects = Subject.objects.all();
+
+    print "forum_subject:",forum_subjects
+    args = {'posts': posts, 'forum_subjects': forum_subjects}
     return render(request, 'index.html', args)
